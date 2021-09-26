b1ff1a96491b1ae75f63cb43558b9d23f085096c
Lab-Game <91430399+Lab-Game@users.noreply.github.com>
2021-09-26 12:39:55 -0700

    Update main.js

--- client/main.js
+++ client/main.js
@@ -1,1 +1,7 @@
-// Main file for client
+function init() {
+  for (var row = 0; row < 3; ++row) {
+    for (var col = 0; col < 3; ++col) {
+      gameboard.rows[row].cells[col].innerHTML = row + "," + col;
+    }
+  }
+}

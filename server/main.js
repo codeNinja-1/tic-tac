8af6bc8e451f4ca26ad3322fcadb08100e9b8d07
Fuzzy Wuzzy <74162951+codeNinja-1@users.noreply.github.com>
2021-09-26 14:12:52 -0700

    Update main.js

--- server/main.js
+++ server/main.js
@@ -49,6 +49,25 @@ class Game {
     this.player1.socket.emit("gameState", this.gameOver ? null : this.turn == this.which(player), this.board);
     this.player2.socket.emit("gameState", this.gameOver ? null : this.turn == this.which(player), this.board);
   }
+  result(board) {
+    for (var i = 0;i < 3;i++) {
+      if (board[i * 3] == board[i * 3 + 1] && board[i * 3] == board[i * 3 + 2] && board[i * 3] != 0) {
+        return board[i * 3];
+      }
+    }
+    for (var i = 0;i < 3;i++) {
+      if (board[i] == board[i + 3] && board[i] == board[i + 6] && board[i] != 0) {
+        return board[i];
+      }
+    }
+    if (board[0] == board[4] && board[0] == board[8] && board[0] != 0) {
+      return board[0];
+    }
+    if (board[2] == board[4] && board[2] == board[6] && board[2] != 0) {
+      return board[2];
+    }
+    return 0;
+  }
 }
 
 var unpaired = [];
